056640b3487141c246cefd260af3c752a5bd052b
Roman Shostak <shostakrrs@gmail.com>
2026-07-07 08:10:09 +0200

    fixed for macos

--- setup.py
+++ setup.py
@@ -2,15 +2,52 @@ from pathlib import Path
 from setuptools import setup, Extension
 import pybind11
 import os
+import sys
 
 cpp_backend = Path(__file__).parent / "cpp_backend"
 
-if os.name == "nt":
-    extra_compile_args = ["/std:c++17", "/openmp"]
-    extra_link_args = []
+env_openmp = os.environ.get("RT_OPENMP")
+env_mac_arch = os.environ.get("RT_MAC_ARCH", "arm64").lower()
+
+if env_openmp is None:
+    USE_OPENMP = True
 else:
-    extra_compile_args = ["-std=c++17", "-fopenmp"]
-    extra_link_args = ["-fopenmp"]
+    USE_OPENMP = env_openmp == "1"
+
+extra_compile_args = []
+extra_link_args = []
+
+if sys.platform == "darwin":
+    libomp_prefix = "/opt/homebrew/opt/libomp"
+
+    if env_mac_arch == "arm64":
+        os.environ["ARCHFLAGS"] = "-arch arm64"
+    elif env_mac_arch == "universal2":
+        os.environ["ARCHFLAGS"] = "-arch arm64 -arch x86_64"
+
+    extra_compile_args = ["-std=c++17"]
+
+    if USE_OPENMP:
+        extra_compile_args += [
+            "-Xpreprocessor",
+            "-fopenmp",
+            f"-I{libomp_prefix}/include",
+        ]
+        extra_link_args += [
+            f"-L{libomp_prefix}/lib",
+            "-lomp",
+        ]
+
+elif os.name == "nt":
+    extra_compile_args = ["/std:c++17"]
+    if USE_OPENMP:
+        extra_compile_args += ["/openmp"]
+
+else:
+    extra_compile_args = ["-std=c++17"]
+    if USE_OPENMP:
+        extra_compile_args += ["-fopenmp"]
+        extra_link_args += ["-fopenmp"]
 
 ext_modules = [
     Extension(
